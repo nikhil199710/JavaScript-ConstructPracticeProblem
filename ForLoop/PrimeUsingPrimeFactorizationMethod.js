bb60453d7435ccc81a07d85f79e29702f0283825
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-26 13:41:22 +0530

    Update PrimeUsingPrimeFactorizationMethod.js

--- ForLoop/PrimeUsingPrimeFactorizationMethod.js
+++ ForLoop/PrimeUsingPrimeFactorizationMethod.js
@@ -5,11 +5,11 @@ let count = 0;
 
 for (let i = 2; i <= Math.sqrt(Number); i++)
     {
-        if (Number % i === 0) 
+        if (Number % i == 0) 
             count++;
     }
 
-if (count === 0) 
+if (count == 0) 
     console.log(Number + " is a prime number");
 else 
     console.log(Number + " is not a prime number");
