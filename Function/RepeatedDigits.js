ec0964aaf44b3a038682d8e5928f19e2c897277f
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-26 15:43:38 +0530

    Update RepeatedDigits.js

--- Function/RepeatedDigits.js
+++ Function/RepeatedDigits.js
@@ -1,8 +1,13 @@
 console.log("Numbers with same digits are : ")
+let array=[];
 for(let i = 1; i<100;i++)
 {
     let ones = i%10;
     let tens = (i - ones)/10;
     if(ones == tens)
-    console.log(i);
+    {
+    array.push(i);
+    }
+
 }
+console.log(array);
